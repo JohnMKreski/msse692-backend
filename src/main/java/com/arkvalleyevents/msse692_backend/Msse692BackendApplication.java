adde3e5ec8a76b46f983c00b6332680c23869708
JohnMKreski <jkreski@regis.edu>
2025-09-29 00:06:58 -0600

    Initial Commit

---
+++ src/main/java/com/arkvalleyevents/msse692_backend/Msse692BackendApplication.java
@@ -0,0 +1,12 @@
+package com.arkvalleyevents.msse692_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msse692BackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Msse692BackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/arkvalleyevents/msse692_backend/Msse692BackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.arkvalleyevents.msse692_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msse692BackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
